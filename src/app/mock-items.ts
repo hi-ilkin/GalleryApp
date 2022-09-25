7707146dbc6aef563f88042993ff29368e9861a0
ilkin <huseynlilkin@gmail.com>
2022-09-25 19:44:42 +0200

    added more mock data

--- src/app/mock-items.ts
+++ src/app/mock-items.ts
@@ -1,22 +1,62 @@
 import { Item } from './image-items';
 
-export const ITEMS: Item[] = [
-  {
-    id: 1,
-    imagePath: 'https://picsum.photos/id/945/900/500',
-  },
-  {
-    id: 2,
-    imagePath: 'https://picsum.photos/id/666/900/600',
-  },
-  {
-    id: 3,
-    imagePath: 'https://picsum.photos/id/10/900/500',
-  },
-  {
-    id: 4,
-    imagePath: 'https://picsum.photos/id/100/1000/500',
-  },
+export const ITEMS: Item[][] = [
+  [
+    {
+      id: 1,
+      imagePath: 'https://picsum.photos/id/945/900/500',
+    },
+    {
+      id: 2,
+      imagePath: 'https://picsum.photos/id/666/900/600',
+    },
+    {
+      id: 3,
+      imagePath: 'https://picsum.photos/id/10/900/500',
+    },
+  ],
+  [
+    {
+      id: 1,
+      imagePath: 'https://picsum.photos/id/100/1000/500',
+    },
+  ],
+  [
+    {
+      id: 1,
+      imagePath: 'https://picsum.photos/id/324/900/500',
+    },
+    {
+      id: 2,
+      imagePath: 'https://picsum.photos/id/43/900/600',
+    },
+    {
+      id: 3,
+      imagePath: 'https://picsum.photos/id/23/900/500',
+    },
+  ],
+  [
+    {
+      id: 1,
+      imagePath: 'https://picsum.photos/id/64/900/500',
+    },
+    {
+      id: 2,
+      imagePath: 'https://picsum.photos/id/435/900/600',
+    },
+    {
+      id: 3,
+      imagePath: 'https://picsum.photos/id/34/900/500',
+    },
+    {
+      id: 4,
+      imagePath: 'https://picsum.photos/id/45/900/500',
+    },
+    {
+      id: 5,
+      imagePath: 'https://picsum.photos/id/56/900/500',
+    },
+  ],
 ];
 
 // better structure
