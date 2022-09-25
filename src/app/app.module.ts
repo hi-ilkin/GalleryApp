bf7309b44d21bfc5f6758de275d0eb034c02e66d
ilkin <huseynlilkin@gmail.com>
2022-09-25 18:17:52 +0200

    display images in single row

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { ImageGroupComponent } from './components/image-group/image-group.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 @NgModule({
   declarations: [AppComponent, ImageItemComponent, ImageGroupComponent],
@@ -17,6 +18,7 @@ import { MatButtonModule } from '@angular/material/button';
     FontAwesomeModule,
     BrowserAnimationsModule,
     MatButtonModule,
+    MatGridListModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
