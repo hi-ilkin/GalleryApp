e229a324e6b6bdfef10d2823aaf9e1408fc91dae
ilkin <huseynlilkin@gmail.com>
2022-09-25 12:25:50 +0200

    moved data to mock

---
+++ src/app/mock-items.ts
@@ -0,0 +1,30 @@
+import { Item } from './image-items';
+
+export const ITEMS: Item[] = [
+  {
+    id: 1,
+    imagePath: 'https://picsum.photos/id/945/900/500',
+  },
+  {
+    id: 2,
+    imagePath: 'https://picsum.photos/id/666/900/600',
+  },
+  {
+    id: 3,
+    imagePath: 'https://picsum.photos/id/10/900/500',
+  },
+];
+
+// better structure
+// IMAGE_ITEMS = [
+//   {
+//     groupId: 1,
+//     items: [
+//       { id: 1, imgPath: '' },
+//       {
+//         id: 2,
+//         imgPath: '',
+//       },
+//     ],
+//   },
+// ];
